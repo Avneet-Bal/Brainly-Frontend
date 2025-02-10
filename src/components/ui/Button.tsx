fc0cf2d6f46fd0e746e7621589bc65c4552ff6ac
Avneet Kaur <avibal1325@gmail.com>
2025-02-10 22:45:52 +0530

    Button ui added

---
+++ src/components/ui/Button.tsx
@@ -0,0 +1,27 @@
+import { ReactElement } from "react";
+
+interface ButtonProps {
+    varient: "primary" | "secondary";
+    size: "sm" | "md" | "lg";
+    text: string;
+    startIcon?: ReactElement;
+    endIcon?: ReactElement;
+    onClick: () => void;
+}
+
+const varientStyles = {
+    "primary": "bg-purple-600 text-white",
+    "secondary": "bg-purple-100 text-purple-600"
+}
+
+const sizeStyles = {
+    "sm": "p-2",
+    "md": "p-4",
+    "lg": "p-6"
+}
+
+const defaultStyles = "rounded-md flex"
+
+export const Button = (props: ButtonProps) => {
+    return <button className={`${varientStyles[props.varient]} ${defaultStyles} ${sizeStyles[props.size]}`}>{props.startIcon} {props.text} {props.endIcon}</button>
+}
